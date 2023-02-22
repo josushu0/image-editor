01a26dcea6e2744758687ed62713006a3f70712d
Josué Martell <josue.martell17@gmail.com>
2023-02-22 00:00:59 -0600

    feat(downloads): added downloads component to sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,18 +1,21 @@
 import { useAppSelector } from "../hooks/redux"
 import { RootState } from "../store/store"
 import Adjustments from "./Adjustments"
+import Downloads from "./Downloads"
 import Images from "./Images"
 
 function SideBar() {
-	const activeAdjustments = useAppSelector((state: RootState) => state.tool.adjustments)
-	const activeImages = useAppSelector((state: RootState) => state.tool.images)
+	const tool = useAppSelector((state: RootState) => state.tool)
 	return (
 		<aside tabIndex={-1} className="h-screen w-80 border-r-2 border-neutral-200 p-4 overflow-x-scroll">
-			{ activeAdjustments &&
+			{ tool.adjustments &&
 				<Adjustments />
 			}
-			{ activeImages &&
+			{ tool.images &&
 				<Images />
+			}
+			{ tool.downloads &&
+				<Downloads />
 			}
 		</aside>
 	)
