62fb8983a3d3526140fd477844ac77d1e53eaf6d
Josué Martell <josue.martell17@gmail.com>
2023-02-21 19:43:22 -0600

    feat: added toolbar, sidebar and preview components to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import Preview from "./components/Preview"
+import SideBar from "./components/SideBar"
+import ToolBar from "./components/Toolbar"
+
 function App() {
   return (
-    <></>
+    <main className="flex font-sans accent-neutral-900">
+      <ToolBar />
+      <SideBar />
+      <Preview />
+    </main>
   )
 }
 
