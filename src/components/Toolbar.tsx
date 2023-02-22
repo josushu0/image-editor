f9fa614d484521c56708370f3505a8650da53696
Josué Martell <josue.martell17@gmail.com>
2023-02-21 23:59:46 -0600

    feat(tools): added downloads tool to toolbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,11 +1,12 @@
-import { AdjustmentsHorizontalIcon, PhotoIcon } from "@heroicons/react/24/solid"
+import { AdjustmentsHorizontalIcon, ArrowDownTrayIcon, PhotoIcon } from "@heroicons/react/24/solid"
 import { useAppDispatch, useAppSelector } from "../hooks/redux"
-import { activateAdjustments, activateImages } from "../store/slices/toolSlice"
+import { activateAdjustments, activateDownloads, activateImages } from "../store/slices/toolSlice"
 import { RootState } from "../store/store"
 
 function ToolBar() {
 	const activeAdjustments = useAppSelector((state: RootState) => state.tool.adjustments)
 	const activeImages = useAppSelector((state: RootState) => state.tool.images)
+	const activeDownloads = useAppSelector((state: RootState) => state.tool.downloads)
 	const dispatch = useAppDispatch()
 
 	return (
@@ -22,6 +23,12 @@ function ToolBar() {
 				className={`p-2 border-2 rounded-xl hover:bg-neutral-200 transition-colors ${activeImages ? "border-neutral-900" : "border-transparent"}`}>
 				<span className="sr-only">Seleccionar imagen</span>
 				<PhotoIcon aria-hidden="true" className="w-6 text-neutral-900" />
+			</button>
+			<button
+				onClick={() => dispatch(activateDownloads())}
+				className={`p-2 border-2 rounded-xl hover:bg-neutral-200 transition-colors ${activeDownloads ? "border-neutral-900" : "border-transparent"}`}>
+				<span className="sr-only">Opciones de descarga</span>
+				<ArrowDownTrayIcon aria-hidden="true" className="w-6 text-neutral-900" />
 			</button>
 		</nav>
 	)
