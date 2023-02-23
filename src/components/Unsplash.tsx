aee4dffe9f579dcc754243e5fb8e410af5255994
Josué Martell <josue.martell17@gmail.com>
2023-02-23 16:13:23 -0600

    feat(images): activate adjustments tool after selecting unsplash image

--- src/components/Unsplash.tsx
+++ src/components/Unsplash.tsx
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux"
 import { Basic } from "unsplash-js/dist/methods/photos/types"
 import { setSrc } from "../store/slices/imageSlice"
+import { activateAdjustments } from "../store/slices/toolSlice"
 
 interface Props {
 	photos: Basic[]
@@ -9,10 +10,15 @@ interface Props {
 function Unsplash({ photos }: Props) {
 	const dispatch = useDispatch()
 
+	const handleClick = (url: string) => {
+		dispatch(setSrc(url))
+		dispatch(activateAdjustments())
+	}
+
 	return (
 		<div className="flex flex-col gap-2">
 			{photos.map((photo) =>
-				<button key={photo.id} onClick={() => dispatch(setSrc(photo.urls.regular))}>
+				<button key={photo.id} onClick={() => handleClick(photo.urls.regular)}>
 					<img src={photo.urls.small} alt={photo.alt_description || "Unsplashed"} />
 				</button>
 			)}
