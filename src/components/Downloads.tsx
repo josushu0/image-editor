7d00bbc8048d3537e73f5cfdbfd2fb364407f31e
Josué Martell <josue.martell17@gmail.com>
2023-02-23 01:23:38 -0600

    feat(downloads): admit crossorigin in downloaded image

--- src/components/Downloads.tsx
+++ src/components/Downloads.tsx
@@ -1,5 +1,5 @@
 import { ChevronDownIcon } from "@heroicons/react/24/solid"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useAppSelector } from "../hooks/redux"
 import { RootState } from "../store/store"
 
@@ -7,6 +7,7 @@ function Downloads() {
 	const [downloadOption, setDownloadOption] = useState('PNG')
 	const [dropdown, setDropdown] = useState(false)
 	const image = useAppSelector((state: RootState) => state.image)
+	let img: HTMLImageElement
 
 	const handleSelection = (selection: string) => {
 		setDownloadOption(selection)
@@ -14,11 +15,9 @@ function Downloads() {
 	}
 
 	const handleDownload = () => {
-		const img = document.createElement('img')
-		img.src = image.src
 		const canvas = document.createElement("canvas")
 		const ctx = canvas.getContext("2d")
-		if(!ctx) return
+		if (!ctx) return
 		canvas.width = img.width
 		canvas.height = img.height
 		ctx.filter = `brightness(${image.brightness / 100}) blur(${image.blur}px) contrast(${image.contrast / 100}) grayscale(${image.grayscale}%) invert(${image.negative}%) saturate(${image.saturation / 100}) sepia(${image.sepia}%)`
@@ -26,32 +25,41 @@ function Downloads() {
 		ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
 
 		const link = document.createElement("a")
-		link.download = `image-edited`
-		if(downloadOption === 'JPG') {
+		link.download = "image-edited"
+		if (downloadOption === 'JPG') {
 			link.href = canvas.toDataURL("image/jpeg")
 		} else {
 			link.href = canvas.toDataURL("image/png")
 		}
 		link.click()
 		link.remove()
-		img.remove()
 		canvas.remove()
 	}
+
+	useEffect(() => {
+		img = document.createElement('img')
+		img.crossOrigin = "Anonymous"
+		img.src = image.src
+
+		return () => {
+			img.remove()
+		}
+	}, [])
 
 	return (
 		<div className="flex flex-col gap-4">
 			<h1 className="text-xl font-bold">Opciones de descarga</h1>
 			<div className="flex flex-col">
 				<div className={`flex border-2 border-neutral-900 transition-all ${dropdown ? 'rounded-none' : 'rounded-xl'}`}>
-					<button onClick={() => handleDownload()} className="p-2 border-r-2 border-neutral-900 grow">{downloadOption}</button>
-					<button onClick={() => setDropdown(!dropdown)} className="flex justify-center items-center p-2">
-						<ChevronDownIcon className="h-6" />	
+					<button onClick={() => handleDownload()} disabled={!image.src} className="p-2 border-r-2 border-neutral-900 grow rounded-l-xl hover:bg-neutral-200 disabled:bg-neutral-200">{downloadOption}</button>
+					<button onClick={() => setDropdown(!dropdown)} className="flex justify-center items-center p-2 rounded-r-xl hover:bg-neutral-200">
+						<ChevronDownIcon className="h-6" />
 					</button>
 				</div>
-				{ dropdown &&
+				{dropdown &&
 					<>
-						<button onClick={() => handleSelection('PNG')} className="p-2 border-2 border-t-0 border-neutral-900">PNG</button>
-						<button onClick={() => handleSelection('JPG')} className="p-2 border-2 border-t-0 border-neutral-900">JPG</button>
+						<button onClick={() => handleSelection('PNG')} className="p-2 border-2 border-t-0 border-neutral-900 hover:bg-neutral-200">PNG</button>
+						<button onClick={() => handleSelection('JPG')} className="p-2 border-2 border-t-0 border-neutral-900 hover:bg-neutral-200">JPG</button>
 					</>
 				}
 			</div>
